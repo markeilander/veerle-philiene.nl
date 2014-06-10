34199247acb06410f26889359451c55214687df0
Eilander <mark.eilander@outlook.com>
2014-06-10 19:52:55 +0000

    merge fix

--- app/models/Cadeau.php
+++ app/models/Cadeau.php
@@ -5,8 +5,8 @@ class Cadeau extends BaseModel {
     protected $table = 'cadeau';
 
     public static $rules = array(
-        'titel'         => 'Required|alphaText|Between:1,20',
-        'omschrijving'  => 'Required|alphaText|Between:1,64',
+        'titel'         => 'Required|alphaText|Between:1,50',
+        'omschrijving'  => 'Required|alphaText|Between:1,128',
         'winkel'        => 'Required|alphaText|Between:1,128',
         'url'           => 'Url|Between:5,512',
         'afbeelding'    => '',
--- app/validators.php
+++ app/validators.php
@@ -2,7 +2,7 @@
 
 //create foo validation rule
 Validator::extend('alphaText', function($field,$value){
-    if(preg_match("/[a-zA-Z0-9&,.()-\s]+/", $value))
+    if(preg_match("/[a-zA-Z0-9?!&,.()-\s]+/", $value))
     {
         return true;
     }
